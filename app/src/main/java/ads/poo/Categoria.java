794f0c5a94f11e0e36f8b6ab8561ada3d7f471e9
Henrique Patricio <henriquepworking@gmail.com>
2024-07-02 21:39:54 -0300

    commit

---
+++ app/src/main/java/ads/poo/Categoria.java
@@ -0,0 +1,12 @@
+package ads.poo;
+
+
+public class Categoria {
+    private String descricao;
+    private int id;
+
+    public Categoria(String descricao, int id) {
+        this.descricao = descricao;
+        this.id = id;
+    }
+}
